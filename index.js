04e25d861e516336bea18523dd705e1dfcff1d91
Pajratos <137279298+pajratos@users.noreply.github.com>
2026-04-21 13:34:56 +0200

    Update index.js
    d

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
-const Wallet = require('./wallet');
+const Wallet = require('./wallett');
 const TransactionMiner = require('./app/transaction-miner');
 
 const isDevelopment = process.env.ENV === 'development';
